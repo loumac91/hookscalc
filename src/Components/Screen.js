821791e8c88c6db737f10a603e97d7aef52513c9
loumac91 <loumac91@googlemail.com>
2018-11-18 12:00:21 +0000

    basic design

--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -2,9 +2,17 @@ import React, { useState } from 'react';
 import './Screen.css';
 
 export default () => {
+
+    const [value, setValue] = useState("123456789");
+
     return (
         <div className="screen">
-            Screen
+            <div className="historyContainer">
+                "placeHolder"
+            </div>
+            <div className="valueContainer">
+                {value}
+            </div>
         </div>
     );
 };
